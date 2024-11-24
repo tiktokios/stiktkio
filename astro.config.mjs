6c7cc61d538845d2c0833af5027d0b89b5eda032
tiktokios <tiktokiocdn@gmail.com>
2024-11-24 19:30:37 +0500

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -23,6 +23,13 @@ export default defineConfig({
   }),
 });
 
+export const onRequest = async (context, next) => {
+  const response = await next();
+  if (response.status === 404) {
+    return context.rewrite("/");
+  }
+  return response;
+}
 
 
 // https://astro.build/config
